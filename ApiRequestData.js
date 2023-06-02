1247f10a12bcda08359f094a58d9e306eaeeeb83
Synthesized Artistry <SynthesizedArtistry@gmail.com>
2023-06-01 21:11:12 -0400

    Add feature
    Gas type filter feature

--- ApiRequestData.js
+++ ApiRequestData.js
@@ -52,39 +52,12 @@ async function GasData(searchData){
     let gasData = await postClient.post(url, data);
 
     gasData = JSON.parse(gasData);
-    console.log(gasData.data.locationBySearchTerm.stations.results);
-    console.log(gasData.data.locationBySearchTerm.trends);
+    //console.log(gasData.data.locationBySearchTerm.stations.results);
+    //console.log(gasData.data.locationBySearchTerm.trends);
     return gasData.data.locationBySearchTerm;
 }
 
-/*
-async function ZipToLatLong(zipCode){
-    let params = "zip=" + zipCode + "&key=17o8dysaCDrgvlc";
-    let url = "https://api.promaptools.com/service/us/zip-lat-lng/get/";
-    let dataJson = await getClient.get(url+"?"+params); 
-    
-    dataJSON = JSON.parse(dataJson);
-    let lat = dataJSON.output[0].latitude;
-    let long = dataJSON.output[0].longitude;
-    return[lat, long];
-}
-
-
-async function dataHandler(locationValue){
-    const [gasData, latlong] = await Promise.allSettled([GasData(locationValue), ZipToLatLong(locationValue)]);
-    if(gasData.status == 'fulfilled' && latlong.status == 'fulfilled') {
-        return [gasData.value, latlong.value];
-    }
-}
-*/
-
-function showMessage(input, message){
-    const msg = input.parentNode.querySelector("#average-low-data-control");
-    console.log(message);
-    msg.innerHTML = message;
-}
-
-const quicksort = (arr) => {
+const quicksort = (arr,fuelType) => {
     if(arr.length <= 1){
         return arr;
     }
@@ -93,75 +66,71 @@ const quicksort = (arr) => {
     let leftArr = [];
     let rightArr = [];
     // ^ set up for divide n conquer 
-
+    
     for(let i = 1; i < arr.length; i++){
-        if(arr[i].prices[0].credit.price < pivot.prices[0].credit.price && arr[i].prices[0].credit.price != 0)
+        if(arr[i].prices[fuelType].credit.price < pivot.prices[fuelType].credit.price 
+            && arr[i].prices[fuelType].credit.price != 0)
             leftArr.push(arr[i]);
         else
             rightArr.push(arr[i]);
     }
 
-    return [...quicksort(leftArr), pivot, ...quicksort(rightArr)];
+    return [...quicksort(leftArr,fuelType), pivot, ...quicksort(rightArr,fuelType)];
 };
 
-const currency = (country) => {
-    return (country == 'US') ? "$" : "¢";
-}
-
 const formInput = document.querySelector("#gasDataInput");
+const formInputFilter = document.querySelector("#fuelTypes");
+const locationInput = document.querySelector(".searchInput");
 const formOutput = document.querySelector("#gasDataOutput");
-formInput.addEventListener("submit", function (event) {
+const currency = (country) => { return (country == 'US') ? "$" : "¢"; }
+var fuelType = 0;
+
+formInputFilter.addEventListener("change", async function(event){
+    event.preventDefault();
+    fuelType = event.target.selectedIndex;
+    
+    /*
+    let gasData = await GasData(locationInput.value);
+    let sortedStationByPrices = quicksort(gasData.stations.results,fuelType);
+    createNavigation(sortedStationByPrices); */
+});
+
+formInput.addEventListener("submit", async function (event) {
     // stop from submission
     event.preventDefault();
-    // validate form 
-    let location = formInput.elements[0]; // maybe redundant?
-    if(location){
-        const outputParent = formOutput.parentNode.querySelector(".stationsOutputNavigation");
-        const trends = formOutput.parentNode.querySelector(".trendsOutput");
-
-        GasData(location.value).then((result) => {
-            
-            /* create trends text */
-            trends.textContent = createTrends(result.trends[0]);
-            /* create station buton navigation */
-            let sortedStationByPrices = quicksort(result.stations.results);
-            createNavigation(sortedStationByPrices, outputParent);
-            console.log(result);
-        });
+    if(locationInput){
+        const gasData = await GasData(locationInput.value);
+        updateGasTrends(gasData.trends[0])
+        const sortedStationByPrices = quicksort(gasData.stations.results,fuelType);
+        createNavigation(sortedStationByPrices);
     }else{
-        alert("error");
+        alert("please enter a location");
     }
 });
 
-function createTrends(trends) {
-    return "Average gas price in " + trends.areaName + " for today is " + trends.today + currency(trends.country) + ". " +
-    "Lowest gas price in " + trends.areaName + " for today is " + trends.todayLow + currency(trends.country); 
+function updateGasTrends(trends) {
+    const trendsOutput = formOutput.parentNode.querySelector(".trendsOutput");
+    trendsOutput.textContent = 
+    `Average gas price in ${trends.areaName} for today is ${trends.today}${currency(trends.country)}. 
+    Lowest gas price in ${trends.areaName} for today is ${trends.todayLow}${currency(trends.country)}.`;
 }
 
-function createNavigation(sortedNavArr, output){
+function createNavigation(sortedStations){
+    const output = formOutput.parentNode.querySelector(".stationsOutputNavigation");
     while(output.hasChildNodes()) { output.removeChild(output.firstChild); }
-    let topBestPrices = 3;
+    const topBestPrices = 3;
     for(let i = 0; i < topBestPrices; i++){
-        let station = sortedNavArr[i];
-        let button = document.createElement('button');
-        let location = station.address;
-        let queryLink = 'https://www.google.com/maps/search/?api=1&query=' + station.name + "%2C " + location.line1.replace(/\s/g,'+') 
-        + "%2C " + location.locality + "%2C " + location.region;
-        
-        button.setAttribute('class','stationInfoNav');
-        button.setAttribute('href',queryLink);
-        button.textContent = station.name + " " + location.line1 + " " + station.prices[0].credit.price + currency(location.country);
-        button.addEventListener("click", navigateListener);
-
+        const station = sortedStations[i];
+        const { name, address, prices } = station;
+        const { line1, locality, region, country } = address;
+        const button = document.createElement('button');
+        const queryLink = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(`${name}, ${line1}, ${locality}, ${region}`)}`;
+        button.classList.add("stationInfoNav");
+        button.href = queryLink;
+        button.textContent = `${name} ${line1} ${prices[fuelType].credit.price}${currency(country)}`;
+        button.addEventListener("click", () => {
+            window.location.href = button.href;
+        });
         output.appendChild(button);
     }
 }
-
-function navigateListener(event){
-    let element = event.target;
-    document.location.href = element.getAttribute('href'); // "https://www.google.com/maps/search/?api=1&query=1702%20SW%208th%20ST";
-}
-
-// curl --request POST --header "content-type: application/json" 
-// --url "https://www.gasbuddy.com/graphql" --data "@gasStationSearchPackage.json"
-// curl -X POST --url "https://charts.gasbuddy.com/ch.gaschart?Country=USA&Crude=t&Period=1&Areas=Miami%2CUSA%2CAverage%2C&Unit=US%20%24%2FG" --output "output.png"
